f5e3677267a0b49b4a4c340248cc5eeb4d0cfdf1
VitaliiLisnychenko <lisnychenkovitalii@gmail.com>
2023-09-21 12:01:59 +0200

    friendList

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css['friend-lst']}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
+        />
+      ))}
+    </ul>
+  );
+};
