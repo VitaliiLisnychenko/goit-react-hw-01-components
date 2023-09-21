a768157093ca3545f79dc2e897e73049a4ab039b
VitaliiLisnychenko <lisnychenkovitalii@gmail.com>
2023-09-21 11:15:12 +0200

    statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css['stat-list']}>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={css.item} key={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
